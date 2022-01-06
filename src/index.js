65dcf72df35f82564fa34a97f07bab1f65c235e3
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-01-07 00:36:04 +0200

    update task #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = 'https://restcountries.com/v2/name/';
+
+const inputEL = document.querySelector('input');
+
+function fetchCountries(name) {}
