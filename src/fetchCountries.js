07c72ca4a5c55ad07584c17771bfcc6a5aba0a46
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-01-07 22:27:10 +0200

    update task #1

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+// function fetchCountries() {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+//   fetch(`${BASE_URL}${refs.inputEl.value}`)
+//     .then(response => response.json())
+//     .then(console.log);
+// }
+
+// export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+// import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-const BASE_URL = 'https://restcountries.com/v2/name/';
 
-const inputEL = document.querySelector('input');
+const refs = {
+  inputEl: document.querySelector('input'),
+  listEl: document.querySelector('ul'),
+  infoEl: document.querySelector('.country-info'),
+};
 
-function fetchCountries(name) {}
+refs.inputEl.addEventListener('input', debounce(fetchCountries, 300));
+
+function fetchCountries() {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+  fetch(`${BASE_URL}${refs.inputEl.value}?fields=name,capital,population,flags,languages`)
+    .then(response => response.json())
+    .then(data => {
+      if (refs.inputEl.value) {
+        console.log(data);
+        const markUp = data
+          .map(
+            country => `<li>
+        <p>${country.name.official}</p>
+        <p>${country.capital}</p>
+        <p>${country.population}</p>
+        <p>${Object.keys(country.languages)}</p>
+        <img src="${country.flags.svg}" width="500" height="300"/>
+      </li>`,
+          )
+          .join('');
+        refs.listEl.innerHTML = markUp;
+      } else {
+        refs.listEl.innerHTML = '';
+      }
+    });
+}
