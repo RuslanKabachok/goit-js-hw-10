726a047379348c2c4c1281fc02d316408dbcdff8
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-01-07 22:29:23 +0200

    upd

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,25 @@
 // function fetchCountries() {
 //   const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-//   fetch(`${BASE_URL}${refs.inputEl.value}`)
+//   fetch(`${BASE_URL}${refs.inputEl.value}?fields=name,capital,population,flags,languages`)
 //     .then(response => response.json())
-//     .then(console.log);
+//     .then(data => {
+//       if (refs.inputEl.value) {
+//         console.log(data);
+//         const markUp = data
+//           .map(
+//             country => `<li>
+//         <p>${country.name.official}</p>
+//         <p>${country.capital}</p>
+//         <p>${country.population}</p>
+//         <p>${Object.keys(country.languages)}</p>
+//         <img src="${country.flags.svg}" width="500" height="300"/>
+//       </li>`,
+//           )
+//           .join('');
+//         refs.listEl.innerHTML = markUp;
+//       } else {
+//         refs.listEl.innerHTML = '';
+//       }
+//     });
 // }
-
-// export { fetchCountries };
