b40eacbbc86414404fa3803be69d847891e7a4a6
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-01-09 17:45:40 +0200

    mvp

--- src/index.js
+++ src/index.js
@@ -13,28 +13,50 @@ const refs = {
 
 refs.inputEl.addEventListener('input', debounce(fetchCountries, 300));
 
-function fetchCountries() {
+function fetchCountries(e) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-  fetch(`${BASE_URL}${refs.inputEl.value}?fields=name,capital,population,flags,languages`)
-    .then(response => response.json())
+  fetch(`${BASE_URL}${e.target.value}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
+      return response.json();
+    })
     .then(data => {
-      if (refs.inputEl.value) {
-        console.log(data);
+      if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      } else if (data.length > 2 && data.length < 10) {
         const markUp = data
           .map(
             country => `<li>
         <p>${country.name.official}</p>
-        <p>${country.capital}</p>
-        <p>${country.population}</p>
-        <p>${Object.keys(country.languages)}</p>
-        <img src="${country.flags.svg}" width="500" height="300"/>
+        <img src="${country.flags.svg}" width="150" height="100"/>
       </li>`,
           )
           .join('');
-        refs.listEl.innerHTML = markUp;
-      } else {
+        refs.listEl.innerHTML = markUp.trim();
+      } else if (data.length === 1) {
+        console.log(data);
+        const markUp = data
+          .map(
+            country => `<li>
+          <p>${country.name.official}</p>
+          <p>${country.capital}</p>
+          <p>${country.population}</p>
+          <p>${Object.keys(country.languages)}</p>
+          <img src="${country.flags.svg}" width="500" height="300"/>
+        </li>`,
+          )
+          .join('');
+        refs.listEl.innerHTML = markUp.trim();
+      } else if (!refs.inputEl.value) {
         refs.listEl.innerHTML = '';
       }
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }
